e34c55da9e9962c52fc873098759dc65cc1b0ebc
gemma <gemma@DESKTOP-2Q2G917>
2022-10-10 16:17:11 +0100

    Add project files.

---
+++ List/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var game = new List<string>();
+            game.Add("Animal Crossing: New Horizons");
+            game.Add("Club Penguin Rewritten");
+            {
+                Console.WriteLine(game.Count);
+                Console.ReadLine();
+            }
+        }
+    }
+}
